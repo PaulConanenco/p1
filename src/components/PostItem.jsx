059f2e2a624cea94d8a3b644d98ca1494c9d2fce
PaulConanenco <paul.conanenco@gmail.com>
2021-02-20 18:01:34 +0200

    v2

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './PostItem.css'
 
 function PostItem(props) {
     const {title, body} = props;
 
     return (
-        <div>
+        <div className="post-item">
             <h3>{ title }</h3>
-            <p>{ body }</p>
+            <p >{ body }</p>
             
         </div>
     );
--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import './UserItem.css'
 
 function UserItem(props) {
     const {name, email, isGoldClient, salariu, logo, deleteUser, id} = props;
 
     return (
-        <div>
+        <div className="user-item">
             <h3>{ name }</h3>
             <p>{ email }</p>
             <p>{ salariu }</p>
-            <img src={process.env.PUBLIC_URL + '/images/' + logo} alt="Logo"/>
+            {/* <img src={process.env.PUBLIC_URL + '/images/' + logo} alt="Logo"/> */}
             { isGoldClient
                 ? <h3>Client GOLD</h3>
                 : null
             }
-            <button onClick={() => deleteUser(id)}>Sterge Utilizator</button>
+            <button  onClick={() => deleteUser(id)}>Sterge Utilizator</button>
         </div>
     );
     
